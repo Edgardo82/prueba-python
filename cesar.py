73b065f40d07b32469818a835417f06afe0fb984
Edgardo <edg.mza@gmail.com>
2021-08-21 20:22:28 -0300

    cambios en el codigo

--- cesar.py
+++ cesar.py
@@ -1,2 +1,27 @@
-clave = input("ingrese clave")
-paso = input ("ingrese paso")
+clave = input("ingrese clave: ")
+paso = int(input ("ingrese paso: "))
+code= " "
+listacesar= list(clave)
+for letra in listacesar:
+    if not letra.isalpha():
+        continue    
+    #if letra == "z":
+    #    code +=chr(ord("a") + (paso-1))
+    numero = ord(letra) + paso
+    if numero > ord("z"): # controlo si pasa de z para volver a sumar desde a
+        numero = numero - ord("z")
+        code += chr(ord("a") + (numero-1))
+        continue
+    code += chr(ord(letra)+ paso)
+
+    
+print(code)
+    
+         
+            
+
+
+
+
+
+   
