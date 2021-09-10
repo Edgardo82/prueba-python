02252b834fc418c78869d4b9920581b18fa208bd
Edgardo <edg.mza@gmail.com>
2021-09-09 23:57:47 -0300

    cambios en el archivo

--- objetos.py
+++ objetos.py
@@ -25,3 +25,26 @@ toyota = Carro("azul", 4, 220, 8)
 print(toyota) 
 Hilux = Camioneta("blanca", 4, 200, 16, 2000)
 print(Hilux)
+
+
+## variables de instacia
+class ClaseEjemplo:
+    def __init__(self, val = 1):
+        self.primera = val
+
+    def setSegunda(self, val):
+        self.segunda = val
+
+
+objetoEjemplo1 = ClaseEjemplo()
+objetoEjemplo2 = ClaseEjemplo(2)
+
+objetoEjemplo2.setSegunda(3)
+
+objetoEjemplo3 = ClaseEjemplo(4)
+objetoEjemplo3.tercera = 5
+
+
+print(objetoEjemplo1.__dict__)
+print(objetoEjemplo2.__dict__)
+print(objetoEjemplo3.__dict__)
